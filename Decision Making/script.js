afe821353226bfca720f1e16b914a3c02ee90041
L C <LF9904@gmail.com>
2025-03-12 23:40:58 -0400

    added else if statments

--- Decision Making/script.js
+++ Decision Making/script.js
@@ -8,3 +8,13 @@ if (!variable) {
     console.log("This variable is false");
 }
 
+// Multiple Conditionals
+const age = 18;
+
+if(age >= 18){
+    console.log("You are an adult");
+}else if(age >= 13){
+    console.log("You are a teenager");
+}else{
+    console.log("You are a child");
+}
