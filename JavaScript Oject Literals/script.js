c347a678f715f67a193c7de8e3ba417440b9cbdf
L C <LF9904@gmail.com>
2025-03-16 22:20:23 -0400

    added nesting arrays & objects

--- JavaScript Oject Literals/script.js
+++ JavaScript Oject Literals/script.js
@@ -15,6 +15,41 @@ const restaurant = {
 };
 
 //YOUR CODE GOES DOWN HERE:
-const fullAddress = restaurant["address" + "city" + "state" + "zipcode"];
+const fullAddress =
+  restaurant["address"] +
+  ", " +
+  restaurant["city"] +
+  ", " +
+  restaurant["state"] +
+  " " +
+  restaurant["zipcode"];
 
-console.log(fullAddress);
+console.log(fullAddress); // 99 Johnson Ave, Brooklyn, NY 11206
+
+// Nesting Arrays & Objects
+
+const comments = [
+  {
+    user: "John",
+    comment: "This is a comment",
+  },
+  {
+    user: "Jane",
+    comment: "This is another comment",
+  },
+];
+
+console.log(comments[0].user);
+
+const comments2 = [
+  {
+    user: "John",
+    comment: "This is a comment",
+  },
+  {
+    user: "Jane",
+    comment: "This is another comment",
+  },
+];
+
+console.log(comments2[0].user);
