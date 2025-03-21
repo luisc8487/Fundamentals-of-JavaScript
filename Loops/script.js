8570b9a1c453765150e564e3656b99339f24d14d
L C <LF9904@gmail.com>
2025-03-21 15:05:39 -0400

    added for loop

---
+++ Loops/script.js
@@ -0,0 +1,16 @@
+// For Loop
+
+// Increment
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+// Decrement
+// for (let i = 10; i > 0; i--) {
+//     console.log(i);
+// }
+
+for (let num = 0; num <= 10; num += 2) {
+    console.log(num);
+}
+
