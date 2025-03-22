e8d39d9198f144adf92c5a4da1c4572fd404ad79
L C <LF9904@gmail.com>
2025-03-22 00:08:57 -0400

    added loop through an array in reverse

--- Loops/script.js
+++ Loops/script.js
@@ -1,35 +1,43 @@
-///////////////// 
+/////////////////
 // For Loop
 /////////////////
 
-///////////////// 
+/////////////////
 // Increment
 /////////////////
 // for (let i = 0; i < 10; i++) {
 //     console.log(i);
 // }
 
-///////////////// 
+/////////////////
 // Decrement
 /////////////////
 // for (let i = 10; i > 0; i--) {
 //     console.log(i);
 // }
 
-///////////////// 
+/////////////////
 // Infinite Loop
 /////////////////
 // for (let i = 1; i >= 10; i++) {
 //     console.log("Hello");
 // }
 
-for (let num = 0; num <= 10; num += 2) {
-    console.log(`Console.log(${num});`);
-}
+// for (let num = 0; num <= 10; num += 2) {
+//   console.log(`Console.log(${num});`);
+// }
 
-// loop through an array
+/////////////////////////
+// Loop through an Array
+////////////////////////
 const arr = ["Bob", "Sally", "Tom"];
-for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
-}
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
 
+///////////////// ////////////////
+// Loop through an Array in Reverse
+///////////////// ///////////////
+for (let i = arr.length - 1; i >= 0; i--) {
+  console.log(arr[i]);
+}
