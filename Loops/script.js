34b7cfb0b79c584c9f6fe7d9a7769c199c2f1fa4
L C <LF9904@gmail.com>
2025-03-22 00:02:06 -0400

    added loop over array

--- Loops/script.js
+++ Loops/script.js
@@ -1,16 +1,35 @@
+///////////////// 
 // For Loop
+/////////////////
 
+///////////////// 
 // Increment
+/////////////////
 // for (let i = 0; i < 10; i++) {
 //     console.log(i);
 // }
 
+///////////////// 
 // Decrement
+/////////////////
 // for (let i = 10; i > 0; i--) {
 //     console.log(i);
 // }
 
+///////////////// 
+// Infinite Loop
+/////////////////
+// for (let i = 1; i >= 10; i++) {
+//     console.log("Hello");
+// }
+
 for (let num = 0; num <= 10; num += 2) {
     console.log(`Console.log(${num});`);
+}
+
+// loop through an array
+const arr = [1, 2, 3, 4, 5];
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
 }
 
