55799fb1a087b263e8e465ea0886501bea22623d
L C <LF9904@gmail.com>
2025-03-23 01:16:49 -0400

    created a todo list with current command lines to build out CRUD

---
+++ Loops/Todo-List/app.js
@@ -0,0 +1,20 @@
+const todos = [];
+let command = prompt("What would you like to do: ");
+
+while (command !== "quit") {
+  if (command === "new") {
+    let = newTodo = prompt("Enter a new todo: ");
+    todos.push(newTodo);
+  } else if (command === "list") {
+    console.log("**********");
+    for (let i = 0; i < todos.length; i++) {
+      console.log(`${i}: ${todos[i]}`);
+    }
+    console.log("**********");
+  } else if (command === "delete") {
+    let index = prompt("Enter an index to delete: ");
+    todos.splice(index, 1);
+  }
+  command = prompt("What would you like to do: ");
+}
+console.log("OK, YOU QUIT THE APP!");
