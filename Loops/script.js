cf0664ef1f57db1250091bf139633d0602536fec
L C <LF9904@gmail.com>
2025-03-22 22:45:42 -0400

    added a guessing game

--- Loops/script.js
+++ Loops/script.js
@@ -30,7 +30,7 @@
 /////////////////////////
 // Loop through an Array
 ////////////////////////
-const arr = ["Bob", "Sally", "Tom"];
+// const arr = ["Bob", "Sally", "Tom"];
 // for (let i = 0; i < arr.length; i++) {
 //     console.log(arr[i]);
 // }
@@ -38,37 +38,60 @@ const arr = ["Bob", "Sally", "Tom"];
 ///////////////// ////////////////
 // Loop through an Array in Reverse
 ///////////////// ///////////////
-for (let i = arr.length - 1; i >= 0; i--) {
-  console.log(arr[i]);
-}
+// for (let i = arr.length - 1; i >= 0; i--) {
+//   console.log(arr[i]);
+// }
 
 ///////////////// ////////////////
 // Nested Loops
 ///////////////// ///////////////
-let str = "LOL";
-for (let i = 0; i < 3; i++) {
-  console.log(`Outter Loop: ${i}`);
-  for (let j = 0; j < 3; j++) {
-    console.log(`Inner Loop: ${str[j]}`);
-  }
-}
+// let str = "LOL";
+// for (let i = 0; i < 3; i++) {
+//   console.log(`Outter Loop: ${i}`);
+//   for (let j = 0; j < 3; j++) {
+//     console.log(`Inner Loop: ${str[j]}`);
+//   }
+// }
 
 ///////////////// ////////////////
 // While Loop
 ///////////////// ///////////////
-let i = 0;
-while (i < 10) {
-  console.log(i);
-  i++;
-}
+// let i = 0;
+// while (i < 10) {
+//   console.log(i);
+//   i++;
+// }
 
 ///////////////// ////////////////
 // Break
 ///////////////// ///////////////
-for (let i = 0; i < 10; i++) {
-    // stop the loop when i is 5
-  if (i === 5) {
-    break;
+// for (let i = 0; i < 10; i++) {
+//     // stop the loop when i is 5
+//   if (i === 5) {
+//     break;
+//   }
+//   console.log(i);
+// }
+
+///////////////// ////////////////
+// Writing a Guessing Game
+///////////////// ///////////////
+
+const theGuess = Math.floor(Math.random() * 4) + 1;
+console.log(`The guess is ${theGuess} to guess`);
+let amountOfGuesses = 0;
+let guess = prompt("Guess a number between 1 and 10");
+while (guess != theGuess) {
+  if (guess > theGuess) {
+    alert("Too high! Enter a new guess:");
+  } else {
+    guess = prompt("Too low! Enter a new guess:");
   }
-  console.log(i);
+  amountOfGuesses++;
+  if (amountOfGuesses === "q") {
+    alert(`You guessed ${amountOfGuesses} of times!`);
+  }
 }
+alert(
+  `You guessed ${amountOfGuesses} times in order to get the correct guess!`
+);
