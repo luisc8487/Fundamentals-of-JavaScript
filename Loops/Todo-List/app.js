43cd61c2751d8bab60deb3f82a231e1f06603e7e
L C <LF9904@gmail.com>
2025-03-23 01:42:53 -0400

    added onload

--- Loops/Todo-List/app.js
+++ Loops/Todo-List/app.js
@@ -1,20 +1,22 @@
-const todos = [];
-let command = prompt("What would you like to do: ");
+window.onload = function () {
+  const todos = [];
+  let command = prompt("What would you like to do: ");
 
-while (command !== "quit") {
-  if (command === "new") {
-    let = newTodo = prompt("Enter a new todo: ");
-    todos.push(newTodo);
-  } else if (command === "list") {
-    console.log("**********");
-    for (let i = 0; i < todos.length; i++) {
-      console.log(`${i}: ${todos[i]}`);
+  while (command !== "quit") {
+    if (command === "new") {
+      let = newTodo = prompt("Enter a new todo: ");
+      todos.push(newTodo);
+    } else if (command === "list") {
+      console.log("**********");
+      for (let i = 0; i < todos.length; i++) {
+        console.log(`${i}: ${todos[i]}`);
+      }
+      console.log("**********");
+    } else if (command === "delete") {
+      let index = prompt("Enter an index to delete: ");
+      todos.splice(index, 1);
     }
-    console.log("**********");
-  } else if (command === "delete") {
-    let index = prompt("Enter an index to delete: ");
-    todos.splice(index, 1);
+    command = prompt("What would you like to do: ");
   }
-  command = prompt("What would you like to do: ");
-}
-console.log("OK, YOU QUIT THE APP!");
+  console.log("OK, YOU QUIT THE APP!");
+};
