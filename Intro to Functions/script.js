87d573efcdaed85148c9d25fae430d7c5cd37170
L C <LF9904@gmail.com>
2025-03-23 22:15:11 -0400

    added comments to my js fie

--- Intro to Functions/script.js
+++ Intro to Functions/script.js
@@ -1,9 +1,10 @@
+// declared a function with no parameters
 function singSong() {
-    console.log("Twinkle, twinkle, little star");
-    console.log("How I wonder what you are");
-    console.log("Up above the world so high");
-    console.log("Like a diamond in the sky");
+  console.log("Twinkle, twinkle, little star");
+  console.log("How I wonder what you are");
+  console.log("Up above the world so high");
+  console.log("Like a diamond in the sky");
 }
-
+// call the function twice
 singSong();
 singSong();
