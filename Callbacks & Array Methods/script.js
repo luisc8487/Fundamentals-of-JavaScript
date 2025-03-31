8159bd3ab5ac2ad07345f5156cd2057cad422df0
L C <LF9904@gmail.com>
2025-03-30 21:03:18 -0400

    add forEach method

---
+++ Callbacks & Array Methods/script.js
@@ -0,0 +1,7 @@
+// The forEach method executes a provided function once for each array element.   
+
+// forEach method
+const numbers = [1, 2, 3, 4, 5];
+numbers.forEach(function (number) {
+  console.log(number * 2); // Output: 2, 4, 6, 8, 10
+}   );
