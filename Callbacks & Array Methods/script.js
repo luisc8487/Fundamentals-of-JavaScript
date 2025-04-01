32458afc6bbc6dcfc8538021bff901fcf651bb22
L C <LF9904@gmail.com>
2025-04-01 00:45:07 -0400

    added map method & description

--- Callbacks & Array Methods/script.js
+++ Callbacks & Array Methods/script.js
@@ -21,3 +21,16 @@ person.forEach((trait) => {
   console.log(`${trait.name} is ${trait.age} years old.`); // Output: John is 30 years old.
   console.log(`${trait.name} lives in ${trait.city}.`); // Output: John lives in New York.
 });
+
+// map method
+
+// The map method creates a new array populated with the results of calling a provided function on every element in the calling array.
+// It is also a higher-order function that takes a callback function as an argument and applies it to each element in the array, returning a new array. 
+
+const numbers2 = [1, 2, 3, 4, 5];
+const doubledNumbers = numbers2.map((number) => number * 2);
+console.log(doubledNumbers); // Output: [2, 4, 6, 8, 10]
+
+person.map((trait) => {
+  console.log(`${trait.name} would be ${trait.age * 2} years old if it was `); // Output: John is 30 years old.
+});
