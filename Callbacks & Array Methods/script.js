74c254c1ee908a36c4458b0c08c9e42ff8d61605
L C <LF9904@gmail.com>
2025-03-31 23:53:06 -0400

    added forEach function with description

--- Callbacks & Array Methods/script.js
+++ Callbacks & Array Methods/script.js
@@ -1,7 +1,9 @@
 // The forEach method executes a provided function once for each array element.   
 
-// forEach method
+// It is a higher-order function that takes a callback function as an argument and applies it to each element in the array.
+
+// forEach method with arrow function
 const numbers = [1, 2, 3, 4, 5];
-numbers.forEach(function (number) {
+numbers.forEach((number) => {
   console.log(number * 2); // Output: 2, 4, 6, 8, 10
 }   );
