4eeff6b67d878986ad04a1ffa1445116919030ec
L C <LF9904@gmail.com>
2025-04-06 20:09:43 -0400

    added the eventlistener

--- DOM Events/script.js
+++ DOM Events/script.js
@@ -1,16 +1,21 @@
 const btn = document.querySelector("#v2");
 
 btn.onclick = () => {
-    console.log("YOU CLICKED ME!");
-    console.log("I HOPE YOU LIKE ME!");
+  console.log("YOU CLICKED ME!");
+  console.log("I HOPE YOU LIKE ME!");
 };
 
 function scream() {
-    console.log("AHHHHHHHHHH!!!");
-    console.log("STOP TOUCHING ME!");
+  console.log("AHHHHHHHHHH!!!");
+  console.log("STOP TOUCHING ME!");
 }
 btn.onmouseenter = scream;
 
 document.querySelector("h1").onclick = () => {
-    alert("YOU CLICKED THE HEADING!");
-}
+  alert("YOU CLICKED THE HEADING!");
+};
+
+const btn3 = document.querySelector("#v3");
+btn3.addEventListener("click", () => {
+    alert("CLICKED!")
+});
