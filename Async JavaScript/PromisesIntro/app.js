5ee133e7dcfe4c200ee5d115003ffa4aa7c90286
L C <LF9904@gmail.com>
2025-04-13 19:51:07 -0400

    added fakeRequest Using Promises

--- Async JavaScript/PromisesIntro/app.js
+++ Async JavaScript/PromisesIntro/app.js
@@ -11,9 +11,9 @@ const fakeRequestCallback = (url, success, failure) => {
 
 const fakeRequestPromise = (url) => {
   return new Promise((resolve, reject) => {
-    const delay = Math.floor(Math.random() * 3000) + 1000; // Random delay between 1 and 4 seconds
+    const delay = Math.floor(Math.random() * 2 + 1) * 1000; // Random delay between 1 and 2 seconds
     setTimeout(() => {
-      if (delay === 4000) {
+      if (delay === 2000) {
         reject("Connection Timeout");
       } else {
         resolve(`Here is your fake data from ${url}`);
@@ -22,36 +22,51 @@ const fakeRequestPromise = (url) => {
   });
 };
 
-fakeRequestCallback(
-  "book.com",
-  (response) => {
-    console.log(response);
+// fakeRequestCallback(
+//   "book.com",
+//   (response) => {
+//     console.log(response);
+//     console.log("1st Data received!");
+//     fakeRequestCallback(
+//       "book.com/page2",
+//       (response) => {
+//         console.log(response);
+//         console.log("2nd Data received!");
+//         fakeRequestCallback(
+//           "book.com/page3",
+//           (response) => {
+//             console.log(response);
+//             console.log("3rd Data received!");
+//           },
+//           (error) => {
+//             console.log(error);
+//             console.log("3rd Error!");
+//           }
+//         );
+//       },
+//       (error) => {
+//         console.log(error);
+//         console.log("2nd Error!");
+//       }
+//     );
+//   },
+//   (error) => {
+//     console.log(error);
+//     console.log("1st Error!");
+//   }
+// );
+
+fakeRequestPromise("yelp.com/api/coffee/page1")
+  .then(() => {
     console.log("1st Data received!");
-    fakeRequestCallback(
-      "book.com/page2",
-      (response) => {
-        console.log(response);
+    fakeRequestPromise("yelp.com/api/coffee/page2")
+      .then(() => {
         console.log("2nd Data received!");
-        fakeRequestCallback(
-          "book.com/page3",
-          (response) => {
-            console.log(response);
-            console.log("3rd Data received!");
-          },
-          (error) => {
-            console.log(error);
-            console.log("3rd Error!");
-          }
-        );
-      },
-      (error) => {
-        console.log(error);
+      })
+      .catch(() => {
         console.log("2nd Error!");
-      }
-    );
-  },
-  (error) => {
-    console.log(error);
-    console.log("1st Error!");
-  }
-);
+      });
+  })
+  .catch(() => {
+    console.log("Error occurred!");
+  });
