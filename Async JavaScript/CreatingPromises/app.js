8d2efe98c9380acc73a120206122701b039f378e
L C <LF9904@gmail.com>
2025-04-13 22:37:13 -0400

    added CreatePromises and Used Promises

---
+++ Async JavaScript/CreatingPromises/app.js
@@ -0,0 +1,23 @@
+// Creating a promise
+// const fakeRequest = (url) => {
+//   return new Promise((resolve, reject) => {
+//     const rand = Math.random();
+//     setTimeout(() => {
+//       if (rand < 0.5) {
+//         resolve("Your fake data here!");
+//       }
+//       reject("Request failed!");
+//     }, 1000);
+//   });
+// };
+
+// Using the promise
+// fakeRequest("/dogs/1")
+//   .then((data) => {
+//     console.log("Request succeeded!");
+//     console.log("Data is: ", data);
+//   })
+//   .catch((error) => {
+//     console.log("Error: ", error);
+//   });
+
