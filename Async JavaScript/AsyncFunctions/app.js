4052c6738fca5e8123993003cc9c7f3436ae4c21
L C <LF9904@gmail.com>
2025-04-14 23:48:51 -0400

    handling errors with Async/await

--- Async JavaScript/AsyncFunctions/app.js
+++ Async JavaScript/AsyncFunctions/app.js
@@ -48,21 +48,46 @@ const delayColorPromise = (newColor, delay) => {
 //   .then(() => delayColorPromise("purple", 1000));
 
 // Using async/await to make the code cleaner
-const rainbow = async () => {
-  await delayColorPromise("red", 1000);
-  await delayColorPromise("orange", 1000);
-  await delayColorPromise("yellow", 1000);
-  await delayColorPromise("green", 1000);
-  await delayColorPromise("blue", 1000);
-  await delayColorPromise("purple", 1000);
-  return "All done!";
+// const rainbow = async () => {
+//   await delayColorPromise("red", 1000);
+//   await delayColorPromise("orange", 1000);
+//   await delayColorPromise("yellow", 1000);
+//   await delayColorPromise("green", 1000);
+//   await delayColorPromise("blue", 1000);
+//   await delayColorPromise("purple", 1000);
+//   return "All done!";
+// };
+
+// // rainbow().then(() => console.log("END OF RAINBOW!"));
+
+// async function printRainbow() {
+//   await rainbow();
+//   console.log("END OF RAINBOW!");
+// }
+
+// printRainbow();
+
+// Handling Errors with Async/Await
+const fakeRequestPromise = (url) => {
+  return new Promise((resolve, reject) => {
+    const delay = Math.floor(Math.random() * 2 + 1) * 1000; // Random delay between 1 and 2 seconds
+    setTimeout(() => {
+      if (delay === 2000) {
+        reject("Connection Timeout");
+      } else {
+        resolve(`Here is your fake data from ${url}`);
+      }
+    }, delay);
+  });
 };
 
-// rainbow().then(() => console.log("END OF RAINBOW!"));
-
-async function printRainbow() {
-  await rainbow();
-  console.log("END OF RAINBOW!");
+async function makeRequest() {
+  try {
+    let data1 = await fakeRequestPromise("yelp.com/api/coffee/page1");
+    console.log(data1);
+    let data2 = await fakeRequestPromise("yelp.com/api/coffee/page2");
+    console.log(data2);
+  } catch (err) {
+    console.log("Caught an error: ", err);
+  }
 }
-
-printRainbow();
