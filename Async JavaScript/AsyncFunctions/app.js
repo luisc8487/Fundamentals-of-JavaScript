e51a2c17c22ac8916f97e8542316f5855f969e95
L C <LF9904@gmail.com>
2025-04-14 22:45:06 -0400

    added async keyword

--- Async JavaScript/AsyncFunctions/app.js
+++ Async JavaScript/AsyncFunctions/app.js
@@ -1,4 +1,13 @@
-const sing = async () => {
-  throw new Error("I am a bad singer!");
-  return "LA LA LA LA";
-};
+// const sing = async () => {
+//   throw new Error("I am a bad singer!");
+//   return "LA LA LA LA";
+// };
+
+// // The function is declared as async, so it returns a promise
+// sing()
+//   .then((data) => {
+//     console.log("Promise resolved with: ", data);
+//   })
+//   .catch((err) => {
+//     console.log("Promise rejected with: ", err.message);
+//   });
