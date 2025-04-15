0b760dbbdad7795b6475e7f9ec205316b8ac6222
L C <LF9904@gmail.com>
2025-04-14 23:44:00 -0400

    added await keyword

--- Async JavaScript/AsyncFunctions/app.js
+++ Async JavaScript/AsyncFunctions/app.js
@@ -13,20 +13,56 @@
 //   });
 
 // The use of Async Keyword
-const login = async (username, password) => {
-  if (!username || !password) {
-    throw new Error("Please provide username and password");
-  }
-  if (password === "luis") {
-    return "Welcome back, Luis!";
-  }
+// const login = async (username, password) => {
+//   if (!username || !password) {
+//     throw new Error("Please provide username and password");
+//   }
+//   if (password === "luis") {
+//     return "Welcome back, Luis!";
+//   }
+// };
+
+// login("lucho", "luis")
+//   .then((msg) => {
+//     console.log("Login successful!");
+//     console.log(msg);
+//   })
+//   .catch((err) => {
+//     console.log("Login failed: ", err.message);
+//   });
+
+const delayColorPromise = (newColor, delay) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      document.body.style.backgroundColor = newColor;
+      resolve();
+    }, delay);
+  });
 };
 
-login("lucho", "luis")
-  .then((msg) => {
-    console.log("Login successful!");
-    console.log(msg);
-  })
-  .catch((err) => {
-    console.log("Login failed: ", err.message);
-  });
+// delayColorPromise("red", 1000)
+//   .then(() => delayColorPromise("orange", 1000))
+//   .then(() => delayColorPromise("yellow", 1000))
+//   .then(() => delayColorPromise("green", 1000))
+//   .then(() => delayColorPromise("blue", 1000))
+//   .then(() => delayColorPromise("purple", 1000));
+
+// Using async/await to make the code cleaner
+const rainbow = async () => {
+  await delayColorPromise("red", 1000);
+  await delayColorPromise("orange", 1000);
+  await delayColorPromise("yellow", 1000);
+  await delayColorPromise("green", 1000);
+  await delayColorPromise("blue", 1000);
+  await delayColorPromise("purple", 1000);
+  return "All done!";
+};
+
+// rainbow().then(() => console.log("END OF RAINBOW!"));
+
+async function printRainbow() {
+  await rainbow();
+  console.log("END OF RAINBOW!");
+}
+
+printRainbow();
