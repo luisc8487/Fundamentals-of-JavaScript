f84278c9ac721730a3586c674f1079c4fcfbe14b
L C <LF9904@gmail.com>
2025-04-17 23:09:02 -0400

    added setting header with axios

--- AJAX and APIs/Axios/app.js
+++ AJAX and APIs/Axios/app.js
@@ -12,13 +12,39 @@
 //   });
 
 // Using async/await with axios
-const getPokemon = async (id) => {
+// const getPokemon = async (id) => {
+//   try {
+//     const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
+//     console.log(res.data);
+//   } catch (error) {
+//     console.log("error", error);
+//   }
+// };
+// getPokemon(1);
+// getPokemon(2);
+
+const btn = document.querySelector("#jokeBtn");
+
+const addNewJoke = async () => {
+  const jokeText = await getDadJoke();
+  console.log(jokeText);
+  const newLi = document.createElement("li");
+  newLi.innerText = jokeText;
+  const ul = document.querySelector("#jokeContainer");
+  ul.append(newLi);
+};
+
+// Setting headers with axios
+const getDadJoke = async () => {
   try {
-    const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
-    console.log(res.data);
+    const config = {
+      headers: {Accept: "application/json"},
+    };
+    const res = await axios.get("https://icanhazdadjoke.com/", config);
+    return res.data.joke;
   } catch (error) {
     console.log("error", error);
   }
 };
-getPokemon(1);
-getPokemon(2);
+
+btn.addEventListener("click", addNewJoke);
