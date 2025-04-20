0e39a307431f639b8e0193ae50ab6885c1db3d4d
L C <LF9904@gmail.com>
2025-04-20 15:39:53 -0400

    added sandbox to practice and reteach the material

--- sandbox.js
+++ sandbox.js
@@ -1,55 +1,22 @@
-//What to do?
+//https://pokeapi.co/api/v2/pokemon/${name}
 
-/*
-- Getting the base key on the iteration function
-- use that key to dictate whether the other element that iteration
-  - if element iteration exist create an array with the element in it
-  - if not add an empty array 
-- make sure this is surround within an object
+const form = document.querySelector("form");
+const input = document.querySelector("#pokemonName");
+const btn = document.querySelector("button");
 
-- HASHMap
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const name = input.value.toLowerCase();
+  fetchPokemon(name);
+});
 
-
-*/
-
-// The Use of a HashMap
-
-// function groupBy(array, iteratee) {
-//   // your code goes here
-//   const hashmap = new Map();
-//   // Getting the base key on the iteration function
-//   for (el of array) {
-//     const key = iteratee(el);
-//     console.log(key);
-
-//     if (!hashmap.has(key)) {
-//       hashmap.set(key, []);
-//     }
-//     // Put the element into the appropriate group
-//     hashmap.get(key).push(el);
-//   }
-//   return hashmap;
-// }
-
-// // debug your code below
-// console.log(groupBy([1.1, 2.2, 2.9], Math.floor));
-
-// The Use of a Regular Object
-
-// function groupBy(array, iteratee) {
-//   // your code goes here
-//   const obj = {};
-
-//   for (let el of array) {
-//     const key = iteratee(el);
-
-//     if (!obj[key]) {
-//       obj[key] = [];
-//     }
-//     obj[key].push(el);
-//   }
-//   return obj;
-// }
-
-// // debug your code below
-// console.log(groupBy([1.1, 2.2, 2.9], Math.floor));
+const fetchPokemon = async (name) => {
+  const url = `https://pokeapi.co/api/v2/pokemon/${name}`;
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+  } catch (error) {
+    console.error("Error fetching Pokemon:", error);
+  }
+};
