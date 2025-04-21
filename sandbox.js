f14ba57b529e2ea6e7de87fd09cd62a10fc871a8
L C <LF9904@gmail.com>
2025-04-21 09:31:15 -0400

    added image

--- sandbox.js
+++ sandbox.js
@@ -16,12 +16,17 @@ const fetchPokemon = async (name) => {
   try {
     const res = await fetch(url);
     const data = await res.json();
+    console.log(data);
     const pokemon = {
       name: data.name,
       id: data.id,
       height: data.height,
       weight: data.weight,
       types: data.types.map((type) => type.type.name).join(", "),
+      sprites: {
+        front_default: data.sprites.front_default,
+        back_default: data.sprites.back_default,
+      },
     };
     console.log(pokemon);
     const li = document.createElement("li");
@@ -31,6 +36,7 @@ const fetchPokemon = async (name) => {
       <p>Height: ${pokemon.height}</p>
       <p>Weight: ${pokemon.weight}</p>
       <p>Types: ${pokemon.types}</p>
+      <img src="${pokemon.sprites.front_default}" alt="${pokemon.name} front" />
     `;
     document.querySelector("#pokemonList").appendChild(li);
   } catch (error) {
