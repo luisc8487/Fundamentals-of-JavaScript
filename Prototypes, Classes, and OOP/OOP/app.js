40068cbf6575859933ac80e7dd436c734bca50f1
L C <LF9904@gmail.com>
2025-04-22 15:37:40 -0400

    added classes

--- Prototypes, Classes, and OOP/OOP/app.js
+++ Prototypes, Classes, and OOP/OOP/app.js
@@ -54,34 +54,66 @@
 // Passes the newly created object from step 1 as the this context
 // Returns this if the function doesn't return its own object
 
-function Color(r, g, b) {
-  this.r = r;
-  this.g = g;
-  this.b = b;
-  console.log(this);
-}
+// function Color(r, g, b) {
+//   this.r = r;
+//   this.g = g;
+//   this.b = b;
+//   console.log(this);
+// }
 
 // Prototype Methods
 // The prototype is an object that is shared by all instances of a constructor function
 // It is a way to add methods to all instances of a constructor function
 
-Color.prototype.rgb = function () {
-  const {r, g, b} = this;
-  return `rgb(${r}, ${g}, ${b})`;
-};
+// Color.prototype.rgb = function () {
+//   const {r, g, b} = this;
+//   return `rgb(${r}, ${g}, ${b})`;
+// };
 
-Color.prototype.hex = function () {
-  const {r, g, b} = this;
-  return (
-    "#" +
-    ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1).toUpperCase()
-  );
-};
+// Color.prototype.hex = function () {
+//   const {r, g, b} = this;
+//   return (
+//     "#" +
+//     ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1).toUpperCase()
+//   );
+// };
 
-Color.prototype.rgba = function (a = 1.0) {
-  const {r, g, b} = this;
-  return `rgba(${r}, ${g}, ${b}, ${a})`;
-};
+// Color.prototype.rgba = function (a = 1.0) {
+//   const {r, g, b} = this;
+//   return `rgba(${r}, ${g}, ${b}, ${a})`;
+// };
 
-const color1 = new Color(255, 100, 25);
-const color2 = new Color(0, 0, 0);
+// const color1 = new Color(255, 100, 25);
+// const color2 = new Color(0, 0, 0);
+
+//////////////////
+// Classes Syntax
+//////////////////
+
+class Color {
+  constructor(r, g, b, name) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
+    this.name = name;
+  }
+  innerRGB() {
+    const {r, g, b} = this;
+    return `${r}, ${g}, ${b}`;
+  }
+  rgb() {
+    return `rgb(${this.innerRGB()})`;
+  }
+  rgba(a = 1.0) {
+    return `rgba(${this.innerRGB()}, ${a})`;
+  }
+  hex() {
+    const {r, g, b} = this;
+    return (
+      "#" +
+      ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1).toUpperCase()
+    );
+  }
+}
+
+const c1 = new Color(255, 67, 89, "tomato");
