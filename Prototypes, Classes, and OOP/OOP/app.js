6b7f6f0954edb34f6c9673e5e30159bf207eb104
L C <LF9904@gmail.com>
2025-04-22 15:54:45 -0400

    added more practice for classes

--- Prototypes, Classes, and OOP/OOP/app.js
+++ Prototypes, Classes, and OOP/OOP/app.js
@@ -96,6 +96,7 @@ class Color {
     this.g = g;
     this.b = b;
     this.name = name;
+    this.calcHSL();
   }
   innerRGB() {
     const {r, g, b} = this;
@@ -113,6 +114,52 @@ class Color {
       "#" +
       ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1).toUpperCase()
     );
+  }
+  hsl() {
+    const {h, s, l} = this;
+    return `hsl(${h}, ${s}%, ${l}%)`;
+  }
+  opposite() {
+    const newHue = (h + 180) % 360;
+    return `${(h + 180) % 360}, ${s}%, ${l}%`;
+  }
+  calcHSL() {
+    let {r, g, b} = this;
+    // Normalize r, g, b to [0, 1] range
+    r /= 255;
+    g /= 255;
+    b /= 255;
+
+    // Find greatest and smallest values
+    let cmin = Math.min(r, g, b),
+      cmax = Math.max(r, g, b),
+      delta = cmax - cmin,
+      h = 0,
+      s = 0,
+      l = 0;
+    if (delta === 0) h = 0;
+    // Red is max
+    else if (cmax === r) h = ((g - b) / delta) % 6;
+    // Green is max
+    else if (cmax === g) h = (b - r) / delta + 2;
+    // Blue is max
+    else h = (r - g) / delta + 4;
+    h = Math.round(h * 60);
+    // Make negative hues positive behind 360°
+    if (h < 0) h += 360;
+
+    // Lightness
+    l = (cmax + cmin) / 2;
+
+    // Saturation
+    s = delta === 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+
+    // Convert to percentage
+    s = +(s * 100).toFixed(1);
+    l = +(l * 100).toFixed(1);
+    this.h = h;
+    this.s = s;
+    this.l = l;
   }
 }
 
